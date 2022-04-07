8a5d7abfe8f35a5d670ac59f62e97d391f91d4c6
Manasi Keskar <manasi@codedata.io>
2022-04-07 14:15:28 -0700

    Initial files to be commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ngx-bootstrap/ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { HeaderComponent } from './containers/header/header.component';
+import { HomeComponent } from './containers/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
